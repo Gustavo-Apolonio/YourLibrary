661854ca44dfd9369a1e85e7bc2daa46a7ccf1f1
Gustavo Apolonio <gustta.apolonio@gmail.com>
2024-06-13 23:54:16 -0300

    chore: improve deploy settings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 5000;
 
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
 app.get('/api', (req: any, res: any) => {
-  res.send('Chat GPT é MUITA vida!');
+  res.send('Chat GPT é muita vida!');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
